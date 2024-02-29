0e743553b770337d7db9adf8eb5a4e0f9bb42c87
Rajib Mahato <rajibmahato0052@gmail.com>
2024-03-01 01:16:04 +0530

    all files are commited

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCi_7sT760rae72Jg8MJS6DIZKOGxlyHnE",
+    authDomain: "challenge-e2e59.firebaseapp.com",
+    projectId: "challenge-e2e59",
+    storageBucket: "challenge-e2e59.appspot.com",
+    messagingSenderId: "812275831140",
+    appId: "1:812275831140:web:1db99f25a093e9a78dea71",
+    measurementId: "G-KHR3HRCFPX"
+  };
